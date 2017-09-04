c8ce9b6e56017748042406f00f19fb527c19802a
Tyler Jurecki <jurecki07@yahoo.com>
2017-09-04 15:16:24 -0500

    intial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const session = require("express-session");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+const sessionConfig = require("./sessionConfig");
+const users = require("./data");
+const checkAuth = require("./middlewares/checkAuth");
+const app = express();
+const port = process.env.PORT || 8000;
+
+// TEMPLATING ENGINE
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionConfig));
+
+app.get("/", (req, res) => {
+  console.log(req.session);
+  res.render("home");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", (req, res) => {
+  let newUser = req.body;
+
+  console.log("newUser: ", newUser);
+  users.push(newUser);
+  console.log("users: ", users);
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  let reqUsername = req.body.username;
+  let reqPassword = req.body.password;
+
+  let foundUser = users.find(user => user.username === reqUsername);
+  if (!foundUser) {
+    return res.render("login", { errors: ["User not found"] });
+  }
+
+if (foundUser.password !== reqPassword) {
+    return res.render("login", {errors: ["Incorrect Password."]});
+}
+delete foundUser.password;
+req.session.user = foundUser;
+res.redirect("/");
+});
+
+app.get("/profile", checkAuth, (req, res) => {
+  res.render("profile", { user: req.session.user });
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}!`);
+});
